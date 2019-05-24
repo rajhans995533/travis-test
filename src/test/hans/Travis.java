6b68447ab17e7786c2f13711f208340217635ef8
hans <rajhans995533@gmail.com>
2019-05-24 16:05:24 +0530

    initial changes

---
+++ src/test/hans/Travis.java
@@ -0,0 +1,24 @@
+/* 
+ * ===========================================================================
+ * File Name Travis.java
+ * 
+ * Created on 24-May-2019
+ *
+ * This code contains copyright information which is the proprietary property
+ * of Chetu India Pvt. Ltd. No part of this code may be reproduced, stored or transmitted
+ * in any form without the prior written permission of CHETU.
+ *
+ * Copyright (C) CHETU. 2019
+ * All rights reserved.
+ *
+ * Modification history:
+ * $Log: Travis.java,v $
+ * ===========================================================================
+ */
+ package test.hans;
+ public class Travis {
+
+     public static void main(String[] args) {
+        System.out.println("Hello Travis App");
+    }
+}
